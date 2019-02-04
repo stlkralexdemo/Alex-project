19580c86f2f27670adebaf0b2d432c8231664642
stlkralexdemo <stlkralex@yandex.ru>
2019-02-04 16:02:53 +0300

    patch n1

--- src/main/java/ru/itpark/alexproject/repository/ProductRepository.java
+++ src/main/java/ru/itpark/alexproject/repository/ProductRepository.java
@@ -13,9 +13,9 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Integer>
 
     List<ProductEntity> findAllByRoadBikeSizeOrderByPriceDesc(Integer roadBikeSize);
 
-    List<ProductEntity> findAllSkateBySkiSizeOrderByPriceDesc(Integer skateSize);
+    List<ProductEntity> findAllSkateBySkiSizeOrderByPriceDesc(Integer skiSize);
 
-    List<ProductEntity> findAllClassicBySkiSizeOrderByPriceDesc(Integer skateSize);
+    List<ProductEntity> findAllClassicBySkiSizeOrderByPriceDesc(Integer skiSize);
 
     List<ProductEntity> findAllByNameContainsIgnoreCaseOrderByPriceDesc(String name);
 
